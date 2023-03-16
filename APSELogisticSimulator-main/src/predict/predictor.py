57a769eddb88a198e6fcfc24fbec028da56651d1
sofiarques <126563384+sofiarques@users.noreply.github.com>
2023-03-16 21:24:31 +0100

    Add files via upload

--- APSELogisticSimulator-main/src/predict/predictor.py
+++ APSELogisticSimulator-main/src/predict/predictor.py
@@ -27,21 +27,24 @@ with open('models/le.pkl', 'rb') as f:
 def predict_eta():
 
     # Get the JSON data from the request body
-    data = request.get_json()['time']
-    prediccion = modelo_tiempo_viaje.predict(data)
+    
+    data = np.array(request.get_json()['time'])
+    prediccion = modelo_tiempo_viaje.predict(data.reshape(1,-1))[0]
+
     # Return the prediction as a JSON response
-    # return jsonify({'prediction': prediccion})
-    return jsonify(prediccion)
+    return jsonify({'prediction': prediccion[0]})
 
 
 # Endpoint for load delivery endpoint.
 # Input is a json object with attributes truckId and time
 @app.route('/predict_delivery', methods=['POST'])
 def predict_delivery():
-    data = np.array(request.get_json()['truckId'])
-    prediccion = modelo_tiempo_entrega.predict(data)
+    data = np.array(request.get_json()['time', 'truckId'])
+    prediccion = modelo_tiempo_entrega.predict(data.reshape(1,-1))[0]
+
     # Return the prediction as a JSON response  
-    return jsonify({'prediction': prediccion})
+    return jsonify({'prediction': prediccion[0]})
 
 if __name__ == '__main__':
+
     app.run(debug=True, port =7777)
